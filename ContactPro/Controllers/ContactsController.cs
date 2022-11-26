5ea1eebd80fb254e7569000622af144170cfe87f
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-11-26 12:18:55 -0600

    Filter Contacts By Category
    1. Wrote code to display all contacts if all contacts is selected
    2. Wrote code to filter the list of contacts based on category

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -34,10 +34,8 @@ namespace ContactPro.Controllers
 
         // GET: Contacts
         [Authorize]
-        public async Task<IActionResult> Index()
+        public IActionResult Index(int categoryId)
         {
-            //var applicationDbContext = _context.Contacts.Include(c => c.AppUser);
-
             var contacts = new List<Contact>();
             string appUserId = _userManager.GetUserId(User);
 
@@ -45,15 +43,26 @@ namespace ContactPro.Controllers
             AppUser appUser = _context.Users
                                       .Include(c => c.Contacts)
                                       .ThenInclude(c => c.Categories)
-                                      .FirstOrDefault(u => u.Id == appUserId);
+                                      .FirstOrDefault(u => u.Id == appUserId)!;
 
             var categories = appUser.Categories;
 
-            contacts = appUser.Contacts.OrderBy(c => c.LastName)
-                                       .ThenBy(c => c.FirstName)
-                                       .ToList();
+            if (categoryId == 0)
+            {
+                contacts = appUser.Contacts.OrderBy(c => c.LastName)
+                                           .ThenBy(c => c.FirstName)
+                                           .ToList();
+            }
+            else
+            {
+                contacts = appUser.Categories.FirstOrDefault(c => c.Id == categoryId)
+                                  .Contacts
+                                  .OrderBy(c => c.LastName)
+                                  .ThenBy(c => c.FirstName)
+                                  .ToList()!;
+            }
 
-            ViewData["CategoryId"] = new SelectList(categories, "Id", "Name");
+            ViewData["CategoryId"] = new SelectList(categories, "Id", "Name", categoryId);
 
             return View(contacts);
         }
