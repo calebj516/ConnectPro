b4d53146b72b551ad2b7c50e1fd9c4c53e0f2773
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-01 22:16:40 -0600

    Category Edit - Email Category GET Action
    1. Built the EmailCategory action
    2. Created and updated the EmailCategory view using code from our template
    3. Created EmailCategoryViewModel

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 using ContactPro.Data;
 using ContactPro.Models;
 using ConnectPro.Models.ViewModels;
+using ConnectPro.Models;
 
 namespace ContactPro.Controllers
 {
@@ -35,6 +36,33 @@ namespace ContactPro.Controllers
                                                 .ToListAsync();
 
             return View(categories);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> EmailCategory(int id)
+        {
+            string appUserId = _userManager.GetUserId(User);
+
+            Category category = await _context.Categories
+                                              .Include(c => c.Contacts)
+                                              .FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
+            List<string> emails = category.Contacts.Select(c => c.Email).ToList();
+
+            EmailData emailData = new EmailData()
+            {
+                GroupName = category.Name,
+                EmailAddress = String.Join(";", emails),
+                Subject = $"Group Message: {category.Name}"
+            };
+
+            EmailCategoryViewModel model = new EmailCategoryViewModel()
+            {
+                Contacts = category.Contacts.ToList(),
+                EmailData = emailData
+            };
+
+            return View(model);
         }
 
         // GET: Categories/Details/5
